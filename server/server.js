2277d03ebd89d8df713062442f180c7a03706a31
fathy2028 <fathy2111262@miuegypt.edu.eg>
2024-07-26 19:28:18 +0200

    ...l

--- server/server.js
+++ server/server.js
@@ -1,55 +1,35 @@
 import express from "express";
 import colors from "colors";
-import dotenv from "dotenv";
-import morgan from "morgan";
-import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
+import dotenv from "dotenv"
+import morgan from "morgan"
+import cors from "cors"
+import path from 'path';
+import { fileURLToPath } from 'url';
 import conn from "./config/db.js";
-import authroutes from "./routes/authroute.js";
-import categoryroute from "./routes/categoryroute.js";
-import productroute from "./routes/productroute.js";
+import authroutes from "./routes/authroute.js"
+import categoryroute from "./routes/categoryroute.js"
+import productroute from "./routes/productroute.js"
 import orderroute from "./routes/orderroute.js";
-
-// Load environment variables
-dotenv.config();
-
-// Initialize express app
-const app = express();
+const app=express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-// Connect to the database
+app.use(express.json())
+app.use(morgan("dev"))
+app.use(cors())
 conn();
-
-// Middleware
-app.use(express.json());
-app.use(morgan("dev"));
-
-// CORS Configuration
-app.use(cors({
-    origin: 'https://cloud-pharmacy.vercel.app',
-    allowedHeaders: 'Content-Type,Authorization'
-}));
-
-// Static files
+dotenv.config();
+//routes
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-// Routes
-app.use("/api/v1/auth", authroutes);
-app.use("/api/v1/category", categoryroute);
-app.use("/api/v1/product", productroute);
+app.use("/api/v1/auth",authroutes);
+app.use("/api/v1/category",categoryroute)
+app.use("/api/v1/product",productroute)
 app.use("/api/v1/order", orderroute);
-
-// Home route
-app.get("/", (req, res) => {
+app.get("/",(req,res)=>{
     res.send({
-        message: "welcome to cloud pharmacy"
-    });
-});
-
-// Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`server running on ${process.env.DEV_MODE} mode on port ${PORT}`.green);
-});
+        message:"welcome to cloud pharmacy"
+    })
+})
+const PORT= process.env.PORT || 8080;
+app.listen(PORT,()=>{
+    console.log(`server running on ${process.env.DEV_MODE} mode ${PORT}`.green);
+})
