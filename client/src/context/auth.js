2c5668b248a367dadc40a9fde72b99d65bba610b
fathy2028 <fathy2111262@miuegypt.edu.eg>
2024-08-13 17:16:02 +0200

    ff

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,33 +1,41 @@
-
 import axios from "axios";
-import { useState , useEffect , useContext , createContext } from "react";
+import { useState, useEffect, useContext, createContext } from "react";
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
-
-const AuthProvider=({children})=>{
-    const [auth,setAuth]=useState({
-        user:null,
-        token:""
-    });
-    axios.defaults.headers.common["Authorization"]=auth?.token
-    useEffect(()=>{
-        const data=localStorage.getItem("auth");
-        if(data){
-            const parseData=JSON.parse(data)
-            setAuth({
-                ...auth,
-                user:parseData.user,
-                token:parseData.token
-            });
+const AuthProvider = ({ children }) => {
+    const [auth, setAuth] = useState(() => {
+        const data = localStorage.getItem("auth");
+        if (data) {
+            const parseData = JSON.parse(data);
+            return {
+                user: parseData.user,
+                token: parseData.token
+            };
+        } else {
+            return {
+                user: null,
+                token: ""
+            };
         }
-        //eslint-disable-next-line
-    },[])
-    return (<AuthContext.Provider value={[auth,setAuth]}>
-    {children}
-    </AuthContext.Provider>
-    );
-}
+    });
 
-const useAuth=()=> useContext(AuthContext);
-export {useAuth,AuthProvider}
+    useEffect(() => {
+        // Set the default Authorization header with the token when auth changes
+        if (auth?.token) {
+            axios.defaults.headers.common["Authorization"] = auth.token;
+        } else {
+            delete axios.defaults.headers.common["Authorization"];
+        }
+    }, [auth]); // This effect runs only when `auth` changes
+
+    return (
+        <AuthContext.Provider value={[auth, setAuth]}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+const useAuth = () => useContext(AuthContext);
+
+export { useAuth, AuthProvider };
