ac45ab6716971d120304fbf73c298ffe59d677a6
fathy2028 <fathy2111262@miuegypt.edu.eg>
2024-08-13 17:54:35 +0200

    trt

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,41 +1,33 @@
+
 import axios from "axios";
-import { useState, useEffect, useContext, createContext } from "react";
+import { useState , useEffect , useContext , createContext } from "react";
 
-const AuthContext = createContext();
+const AuthContext = createContext()
 
-const AuthProvider = ({ children }) => {
-    const [auth, setAuth] = useState(() => {
-        const data = localStorage.getItem("auth");
-        if (data) {
-            const parseData = JSON.parse(data);
-            return {
-                user: parseData.user,
-                token: parseData.token
-            };
-        } else {
-            return {
-                user: null,
-                token: ""
-            };
-        }
+
+const AuthProvider=({children})=>{
+    const [auth,setAuth]=useState({
+        user:null,
+        token:""
     });
-
-    useEffect(() => {
-        // Set the default Authorization header with the token when auth changes
-        if (auth?.token) {
-            axios.defaults.headers.common["Authorization"] = auth.token;
-        } else {
-            delete axios.defaults.headers.common["Authorization"];
+    useEffect(()=>{
+        const data=localStorage.getItem("auth");
+        if(data){
+            const parseData=JSON.parse(data)
+            setAuth({
+                ...auth,
+                user:parseData.user,
+                token:parseData.token
+            });
+            axios.defaults.headers.common["Authorization"]=auth?.token
         }
-    }, [auth]); // This effect runs only when `auth` changes
-
-    return (
-        <AuthContext.Provider value={[auth, setAuth]}>
-            {children}
-        </AuthContext.Provider>
+        //eslint-disable-next-line
+    },[])
+    return (<AuthContext.Provider value={[auth,setAuth]}>
+    {children}
+    </AuthContext.Provider>
     );
-};
+}
 
-const useAuth = () => useContext(AuthContext);
-
-export { useAuth, AuthProvider };
+const useAuth=()=> useContext(AuthContext);
+export {useAuth,AuthProvider}
